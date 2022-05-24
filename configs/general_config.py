415413f4f64ba058c8c368430a9097eb1ca494f1
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-05-24 20:09:24 +0800

    Create general_config.py

---
+++ configs/general_config.py
@@ -0,0 +1,13 @@
+class GeneralConfig:
+    bootstrap_bucket = 'your-cdk'
+    ec2_key_name = 'your_aws_pem'
+    vpc_id = 'vpc-xxxxx'
+    security_group = [
+        'sg-xxxxx'
+    ]
+    
+    subnet_ids = [
+        'subnet-xxxxx', 
+        'subnet-xxxxx'
+    ]
+    
